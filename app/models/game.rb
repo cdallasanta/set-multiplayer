5ca95844bcd0fa57ccae7f32bec855506ba27fb1
cdallasanta <chris.dallasanta@gmail.com>
2020-05-18 10:23:49 -0700

    removed debugger

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,11 +8,10 @@ class Game < ApplicationRecord
     self.deck = *(0..80)
     self.deck.shuffle!
     self.board = self.deck.shift(9)
-    byebug
-    # self.room = SecureRandom.hex 2
-    # while !self.valid? do
-    #   self.room = SecureRandom.hex 2
-    # end
+    self.room = SecureRandom.hex 2
+    while !self.valid? do
+      self.room = SecureRandom.hex 2
+    end
   end
   
 end
