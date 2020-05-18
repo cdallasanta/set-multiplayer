d63864b60450d4e02893fbb257fad5e58429b8d5
cdallasanta <chris.dallasanta@gmail.com>
2020-05-18 10:14:54 -0700

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_165125) do
+ActiveRecord::Schema.define(version: 2020_05_18_171348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_05_18_165125) do
   create_table "games", force: :cascade do |t|
     t.integer "board", array: true
     t.integer "deck", array: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.integer "score", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
