c6a1c77c5614f5a49e0fcd2c2cf2117230ea5785
cdallasanta <chris.dallasanta@gmail.com>
2020-05-18 10:24:54 -0700

    added foreign key to player model

--- db/migrate/20200518171348_create_players.rb
+++ db/migrate/20200518171348_create_players.rb
@@ -3,6 +3,7 @@ class CreatePlayers < ActiveRecord::Migration[6.0]
     create_table :players do |t|
       t.string :name
       t.integer :score, default: 0
+      t.integer :game_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_05_18_171348) do
   create_table "players", force: :cascade do |t|
     t.string "name"
     t.integer "score", default: 0
+    t.integer "game_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
