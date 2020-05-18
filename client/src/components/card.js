9764cd279d400b1ab53b625066e5ef15102d9077
cdallasanta <chris.dallasanta@gmail.com>
2020-05-18 14:52:11 -0700

    cards show up styled!

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import '../stylesheets/cards.scss';
 
-const Card = (props) => {
-  // shapes = () => {
-  //   let shapes = [];
-  //   for (i = 0; i< number; i++){
-  //     shapes += <div className={`${color} ${shape} ${shading}`}></div>
-  //   }
-  //   return shapes;
-  // }
-  debugger;
+const Card = ({data: {id, color, number, shape, shading}}) => {
+  const showShapes = () => {
+    let shapesArr = [];
+    for (let i = 0; i < number; i++){
+      shapesArr.push(<div className={`${color} ${shape} ${shading}`} key={i} />)
+    }
+    return shapesArr;
+  }
   
   return (
     <div className={`card-holder`} id={0}>
-      {/* {showShapes()} */}
+      {showShapes()}
     </div>
   )
 }
